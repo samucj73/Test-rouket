5144d544b2b7f2e5723961f31274f28db5a373b7
samucj73 <samu.rcj@gmail.com>
2025-07-08 00:43:23 -0300

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 TOKEN = "7900056631:AAHjG6iCDqQdGTfJI6ce0AZ0E2ilV2fV9RY"
+url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 
-res = requests.get(f"https://api.telegram.org/bot{7900056631:AAHjG6iCDqQdGTfJI6ce0AZ0E2ilV2fV9RY
-}/getUpdates")
-st.json(res.json())
+res = requests.get(url)
+print(res.json())
