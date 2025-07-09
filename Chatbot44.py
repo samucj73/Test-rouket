b4412451c9495170c2437fd0c50e8ed97c66de83
samucj73 <samu.rcj@gmail.com>
2025-07-08 21:15:49 -0300

    Update Chatbot44.py

--- Chatbot44.py
+++ Chatbot44.py
@@ -270,7 +270,7 @@ with st.expander("📊 Desempenho"):
 
 # 📜 Últimos números
 with st.expander("📜 Últimos Números"):
-    ultimos = [str(h["number"]) for h in st.session_state.historico[-8:]]
+    ultimos = [str(h["number"]) for h in st.session_state.historico[-5:]]
     st.code(" | ".join(ultimos), language="text")
 
 # 📥 Baixar histórico
