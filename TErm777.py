1dd12f0eab8b1b3dbb3f9e00282229d2493d69a9
samucj73 <samu.rcj@gmail.com>
2025-07-10 21:58:04 -0300

    Update TErm777.py

--- TErm777.py
+++ TErm777.py
@@ -77,7 +77,7 @@ if not st.session_state.historico or numero != st.session_state.historico[-1]:
 # Interface
 st.title("🎯 Estratégia de Terminais com Vizinhos (Auto)")
 st.subheader("📥 Últimos Números Sorteados:")
-st.write(list(st.session_state.historico)[-20:])
+st.write(list(st.session_state.historico)[-15:])
 
 # Histórico completo
 historico = list(st.session_state.historico)
