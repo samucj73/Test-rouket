4139c775eeafe095868149cfc15d369e3d827e91
samucj73 <samu.rcj@gmail.com>
2025-08-12 09:22:30 -0300

    Update FDAA.py

--- FDAA.py
+++ FDAA.py
@@ -11,8 +11,8 @@ import threading
 
 # === CONFIGURAÇÕES ===
 API_URL = "https://api.casinoscores.com/svc-evolution-game-events/api/xxxtremelightningroulette/latest"
-TELEGRAM_TOKEN = "TOKEN_AQUI"
-TELEGRAM_CHAT_ID = "CHAT_ID_AQUI"
+TELEGRAM_TOKEN = "7900056631:AAHjG6iCDqQdGTfJI6ce0AZ0E2ilV2fV9RY"
+TELEGRAM_CHAT_ID = "-1002880411750"
 HISTORICO_PATH = Path("historico.pkl")
 ESTADO_PATH = Path("estado.pkl")
 PROB_MINIMA = 0.25
