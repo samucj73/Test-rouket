78e7fbbda31c806b55d01635dc1d03076f9d55a9
samucj73 <samu.rcj@gmail.com>
2025-08-22 09:37:49 -0300

    Update Too2san.py

--- Too2san.py
+++ Too2san.py
@@ -6,7 +6,7 @@ from collections import deque, Counter
 from pathlib import Path
 from streamlit_autorefresh import st_autorefresh
 from catboost import CatBoostClassifier
-
+import Time
 # =========================
 # CONFIGURAÇÕES
 # =========================
@@ -331,7 +331,7 @@ if numero is not None and (st.session_state.ultimo_numero_salvo is None or numer
             if (not st.session_state.ultima_entrada) or reenvio_forcado or chave != st.session_state.ultima_entrada.get("chave"):
                 entrada_obj = {"tipo": tipo, "classes": classes_probs, "chave": chave}
                 txt = f"📊 <b>ENT {tipo}</b>: " + ", ".join(f"{c} ({p*100:.1f}%)" for c,p in classes_probs)
-                enviar_telegram(txt)
+                enviar_telegram(txt, delay=5)
                 st.session_state.ultima_entrada = entrada_obj
                 st.session_state.contador_sem_envio = 0
                 st.session_state._alerta_enviado_rodada = True
