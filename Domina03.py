13614cb1bffc08940eaa1dfcf0a3f051fcb2255b
samucj73 <samu.rcj@gmail.com>
2025-09-09 01:41:00 -0300

    Update Domina03.py

--- Domina03.py
+++ Domina03.py
@@ -122,7 +122,7 @@ class EstrategiaRoleta:
             if n not in self.roleta:
                 continue
             idx = self.roleta.index(n)
-            for offset in range(-3, 4):
+            for offset in range(-4, 5):
                 vizinho = self.roleta[(idx + offset) % len(self.roleta)]
                 conjunto.add(vizinho)
         return conjunto
