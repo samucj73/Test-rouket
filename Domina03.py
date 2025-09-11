04ef72ff90ebece19fd1bb788901d64fd8bc31c6
samucj73 <samu.rcj@gmail.com>
2025-09-11 13:10:19 -0300

    Update Domina03.py

--- Domina03.py
+++ Domina03.py
@@ -188,7 +188,7 @@ if resultado and resultado.get("timestamp") != ultimo_ts:
     st.session_state.contador_rodadas += 1
 
     # Gera nova previsão a cada 2 rodadas
-    if st.session_state.contador_rodadas % 2 == 0:
+    if st.session_state.contador_rodadas % 1 == 0:
         prox_numeros = st.session_state.ia_recorrencia.prever(st.session_state.estrategia.historico)
         if prox_numeros:
             st.session_state.previsao = prox_numeros
