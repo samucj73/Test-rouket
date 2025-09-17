1c0d652621f207cc19c8970b47e191d1c91b85b5
samucj73 <samu.rcj@gmail.com>
2025-09-17 14:23:04 -0300

    Update Domina03.py

--- Domina03.py
+++ Domina03.py
@@ -488,7 +488,10 @@ if resultado and resultado.get("timestamp") != ultimo_ts:
 
             #enviar_telegram("🎯 NP: " + " ".join(str(n) for n in prox_numeros))
             #enviar_telegram("🎯 NP: " + " ".join(str(n) for n in sorted(prox_numeros)))
-            enviar_telegram("🎯 NP: " + " ".join(str(n) for n in (s:=sorted(prox_numeros))[:5]) + ("\n" + " ".join(str(n) for n in s[5:]) if len(s) > 5 else ""))
+            #enviar_telegram("🎯 NP: " + " ".join(str(n) for n in (s:=sorted(prox_numeros))[:5]) + ("\n" + " ".join(str(n) for n in s[5:]) if len(s) > 5 else ""))
+            enviar_telegram("🎯 NP: " + " ".join(map(str, sorted(prox_numeros)[:5])) + ("\n" + " ".join(map(str, sorted(prox_numeros)[5:])) if len(prox_numeros) > 5 else ""))
+            
+            
             
             enviar_telegram_topN("Top N : " + " ".join(str(n) for n in sorted(entrada_topN)))
     else:
