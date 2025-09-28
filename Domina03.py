e0f99bd5d036f9ec4f4cab17b8581b2be60d7d01
samucj73 <samu.rcj@gmail.com>
2025-09-28 14:30:42 -0300

    Update Domina03.py

--- Domina03.py
+++ Domina03.py
@@ -509,7 +509,7 @@ if resultado:
         resultado = None
 
 #if resultado and resultado.get("timestamp") != ultimo_ts and (st.session_state.estrategia.historico == [] or round_id != (st.session_state.estrategia.historico[-1].get("id"))):
- if resultado and resultado.get("timestamp") != ultimo_ts:
+    if resultado and resultado.get("timestamp") != ultimo_ts:
     if not st.session_state.estrategia.historico or round_id != st.session_state.estrategia.historico[-1].get("id"):
         # sua lógica aqui
     # padroniza o dict salvo em memória
