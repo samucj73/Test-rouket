cfbc83e51bf8308a19069657acf425b3dd08ac89
samucj73 <samu.rcj@gmail.com>
2025-10-18 00:26:47 -0300

    Fix file opening mode in carregar_cache function

--- Mlsapi.py
+++ Mlsapi.py
@@ -48,7 +48,7 @@ def salvar_cache(liga_code, ano, dados):
 
 def carregar_cache(liga_code, ano):
     if os.path.exists(cache_file(liga_code, ano)):
-        with open(cache_file(liga_code, "r", encoding="utf-8") as f:
+        with open(cache_file(liga_code, ano), "r", encoding="utf-8") as f:
             return json.load(f)
     return []
 
