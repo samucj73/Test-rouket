ff1de7d4a9b69c8e14c64103414ba828ec0a372f
samucj73 <samu.rcj@gmail.com>
2025-11-20 02:38:40 -0300

    Update Number10.py

--- Number10.py
+++ Number10.py
@@ -2397,7 +2397,7 @@ class EstrategiaML:
             'eficiencia_por_tipo': {},
             'historico_validacao': []
         }
-        logging.info("🔄 Padrões sequenciais e métricas zerados"
+        logging.info("🔄 Padrões sequenciais e métricas zerados")
 
 # =============================
 # SISTEMA DE GESTÃO ATUALIZADO COM ROTAÇÃO POR 3 ACERTOS EM COMBINAÇÕES
